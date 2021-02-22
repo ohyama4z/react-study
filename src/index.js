16545d87859e6e8528636f2ba79a16e186fc38fc
ohayama4z <45777685+ohyama4z@users.noreply.github.com>
2021-02-23 01:31:21 +0900

    シート作成

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<h1>Hello React!!</h1>, document.getElementById("root"));
+class Square extends React.Component {
+  render() {
+    return <div className="square"></div>;
+  }
+}
+
+class Sheet extends React.Component {
+  renderNSquare(squareNum) {
+    return [...Array(squareNum)].map((_, i) => <Square key={i} />);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Sheet1</div>
+        <div className="sheet">{this.renderNSquare(10)}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Sheet />, document.getElementById("root"));
