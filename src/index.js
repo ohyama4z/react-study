cea155b4650563fa66cf60cab066a03b6dd18312
ohayama4z <45777685+ohyama4z@users.noreply.github.com>
2021-02-23 13:53:13 +0900

    SquareコンポーネントをBoardコンポーネントから制御

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../index.css";
+import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+      </button>
+    );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    // 値渡しのslice (slice()はslice(0, arr.length)と同じ)
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
