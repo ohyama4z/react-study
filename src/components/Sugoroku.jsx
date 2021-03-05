356d3794fd037f6fcf081eb90b269b1c3277410c
ohayama4z <45777685+ohyama4z@users.noreply.github.com>
2021-03-05 19:59:35 +0900

    フォーム入力まで

--- src/components/Sugoroku.jsx
+++ src/components/Sugoroku.jsx
@@ -1,52 +1,152 @@
 import React from "react";
-import "../index.css";
+import "./Sugoroku.css";
 
-class Square extends React.Component {
-  render() {
-    return <div className="sugorokuSquare"></div>;
-  }
-}
+const Square = (props) => {
+  return (
+    <div className="square">
+      {props.index}
+      <div className="playerName">{props.playerName}</div>
+    </div>
+  );
+};
 
-class Sheet extends React.Component {
-  renderNSquare(squareNum) {
-    return [...Array(squareNum).keys()].map((index) => (
-      <div key={`${index}`}>
-        <Square index={index + 1} />
-      </div>
-    ));
-  }
-
-  render() {
-    return (
-      <div>
-        <div>Sheet1</div>
-        <div className="sugrokuSheet">{this.renderNSquare(5)}</div>
-      </div>
-    );
-  }
-}
-
-class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          名前を入力: <input type="text" />
+const Board = (props) => {
+  const renderNSquare = (squareNum) => {
+    return [...Array(squareNum).keys()].map((index) => {
+      const playerName =
+        props.player.position === index ? props.player.name : null;
+      return (
+        <div key={`${index}`}>
+          <Square index={index} playerName={playerName} />
         </div>
+      );
+    });
+  };
+
+  return <div className="board">{renderNSquare(props.squareNum)}</div>;
+};
+
+const Form = (props) => {
+  const nameForm = props.players.map(({ name }, i) => (
+    <div key={i}>
+      <input
+        type="text"
+        value={name}
+        onChange={(event) => props.handleChangePlayer(i, event)}
+      />
+    </div>
+  ));
+
+  return (
+    <div>
+      <div>
+        コースの長さを入力
         <div>
-          コースの長さを入力: <input type="text" />
+          <input
+            type="number"
+            value={props.boardLength}
+            onChange={(event) => props.handleChangeLength(event)}
+          />
         </div>
       </div>
-    );
-  }
-}
+      <div>
+        名前を入力
+        {nameForm}
+      </div>
+      <div>
+        <button onClick={() => props.addPlayer()}>プレイヤーを追加</button>
+        <button onClick={() => props.startGame()}>ゲームスタート</button>
+      </div>
+    </div>
+  );
+};
 
 export default class Sugoroku extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [
+        {
+          name: "",
+          position: 0,
+        },
+      ],
+      boardLength: null,
+      isStarted: false,
+    };
+
+    this.handleChangePlayer = this.handleChangePlayer.bind(this);
+    this.handleChangeLength = this.handleChangeLength.bind(this);
+  }
+
+  renderBoards() {
+    return this.state.players.map((player, i) => {
+      return (
+        <div>
+          <div>{`${i + 1}: ${player.name}`}</div>
+          <Board squareNum={this.state.boardLength} player={player} />
+        </div>
+      );
+    });
+  }
+
+  addPlayer() {
+    this.setState({
+      players: [...this.state.players, { name: "", position: 0 }],
+    });
+  }
+
+  handleChangePlayer(i, event) {
+    const playersCopy = this.state.players.slice();
+    playersCopy[i].name = event.target.value;
+    this.setState({
+      players: playersCopy,
+    });
+  }
+
+  handleChangeLength(event) {
+    this.setState({
+      boardLength: Number(event.target.value),
+    });
+  }
+
+  startGame() {
+    this.setState({ isStarted: true });
+  }
+
+  endGame() {
+    this.setState({
+      players: [
+        {
+          name: "",
+          position: 0,
+        },
+      ],
+      boardLength: null,
+      isStarted: false,
+    });
+  }
+
   render() {
+    if (this.state.isStarted) {
+      return (
+        <div>
+          <button onClick={() => this.endGame()}>ゲームを終える</button>
+          <div className="boards">{this.renderBoards()}</div>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <Form />
-        <Sheet />
+        <Form
+          handleChangePlayer={this.handleChangePlayer}
+          handleChangeLength={this.handleChangeLength}
+          players={this.state.players}
+          boardLength={this.state.boardLength}
+          addPlayer={() => this.addPlayer()}
+          startGame={() => this.startGame()}
+        />
       </div>
     );
   }
--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../index.css";
+import "./Tutorial.css";
 
 function Square(props) {
   return (
